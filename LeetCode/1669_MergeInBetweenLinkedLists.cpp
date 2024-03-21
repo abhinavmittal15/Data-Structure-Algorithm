011f17e115af03dff7af0a4935e7270d4fe008d7
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-03-21 21:46:05 +0530

    leetcode  20- march- 2024

---
+++ LeetCode/1669_MergeInBetweenLinkedLists.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class ListNode {
+    public:
+    int val;
+    ListNode* next;
+    ListNode(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+class Solution{
+public:
+    ListNode *mergeInBetween(ListNode *list1, int a, int b, ListNode *list2){
+        ListNode *nodeBeforeA = list1;
+        for (int i = 0; i < a - 1; ++i)
+            nodeBeforeA = nodeBeforeA->next;
+
+        ListNode *nodeB = nodeBeforeA->next;
+        for (int i = 0; i < b - a; ++i)
+            nodeB = nodeB->next;
+
+        nodeBeforeA->next = list2;
+        ListNode *lastNodeInList2 = list2;
+
+        while (lastNodeInList2->next != nullptr)
+            lastNodeInList2 = lastNodeInList2->next;
+
+        lastNodeInList2->next = nodeB->next;
+        nodeB->next = nullptr;
+        return list1;
+    }
+};
