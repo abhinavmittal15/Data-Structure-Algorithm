346cfe8cc9d81552ba58a233fcb1f741376623ac
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-03-31 02:38:01 +0530

    bubble sort coding ninjas

---
+++ CodingNinjas/BubbleSort.cpp
@@ -0,0 +1,26 @@
+//Link to the problem : naukri.com/code360/problems/bubble-sort_980524
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i=1 ; i < n ; i++){
+        for(int j=0 ; j < n-i ; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int n = 5;
+    vector<int> arr = {6 ,2 , 8 , 4 , 10};
+    bubbleSort(arr , n);
+    for(int i=0 ; i < n ; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
