2f3ce84225626d83cd52cd277be309207ba1c2eb
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-03-31 02:45:13 +0530

    made more optimal

--- CodingNinjas/BubbleSort.cpp
+++ CodingNinjas/BubbleSort.cpp
@@ -7,11 +7,15 @@ using namespace std;
 void bubbleSort(vector<int>& arr, int n)
 {   
     for(int i=1 ; i < n ; i++){
+        bool swapped = false;
         for(int j=0 ; j < n-i ; j++){
             if(arr[j] > arr[j+1]){
                 swap(arr[j] , arr[j+1]);
+                swapped = true;
             }
         }
+        if(swapped == false)
+            break;
     }
 }
 
