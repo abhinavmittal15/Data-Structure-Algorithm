ba2c27e6cc5765704fee57a9296f6d262c3b638b
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-04-02 01:03:23 +0530

    58 leetcode

---
+++ LeetCode/58_LengthOfLastWord.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+    public:
+    int lengthOfLastWord(string s){
+        int n = s.size();
+        int count = 0;
+        for(int i = n-1; i >= 0; i--){
+            if(s[i] == ' '){
+                if(count > 0){
+                    return count;
+                }
+            }
+            else{
+                count++;
+            }
+        }
+        // If the last word is very long and there is no space after that, we need to return count directly.
+        return count;        
+    }
+};
+
+int main(){
+    Solution obj;
+    string s = "Hello World";
+    string m = "   fly me   to   the moon  ";
+    cout << obj.lengthOfLastWord(s) << endl;
+    cout << obj.lengthOfLastWord(m) << endl;
+    return 0;
+}
