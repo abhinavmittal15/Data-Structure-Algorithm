bf1a22e5fd8e8275477786b057f32714dc5eecc8
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-04-08 16:55:24 +0530

    new

---
+++ LeetCode/88_MergeSortedArray.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n){
+
+        vector<int> nums3;
+        int i = 0, j = 0;
+        while (i < m && j < n){
+            if (nums1[i] < nums2[j]){
+                nums3.push_back(nums1[i]);
+                i++;
+            }
+            else{
+                nums3.push_back(nums2[j]);
+                j++;
+            }
+        }
+        while(i < m){
+            nums3.push_back(nums1[i]);
+            i++;
+        }
+        while( j < n){
+            nums3.push_back(nums2[j]);
+            j++;
+        }
+    }
+};
+
+
+int main(){
+    vector<int> nums1 = {1,2,3,0,0,0};
+    int m = 3;
+    vector<int> nums2 = {2,5,6};
+    int n = 3;
+
+    Solution S;
+
+    S.merge(nums1, m, nums2, n);
+
+    for(auto i : nums1) 
+        cout << i <<" ";
+    
+    return 0;
+}
