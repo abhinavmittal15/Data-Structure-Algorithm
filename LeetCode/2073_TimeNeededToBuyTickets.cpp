215bda4e930c2ab8583c892b655a2266edaf515d
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-04-10 02:41:48 +0530

    9-4-24-tuesday-leetcode

---
+++ LeetCode/2073_TimeNeededToBuyTickets.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int timeRequiredToBuy(vector<int> &tickets, int k)
+    {
+        int ans = 0;
+
+        for (int i = 0; i < tickets.size(); i++)
+            if (i <= k)
+                ans += min(tickets[i], tickets[k]);
+            else
+                ans += min(tickets[i], tickets[k] - 1);
+
+        return ans;
+    }
+};
+
+int main(){
+    Solution S;
+    vector<int> tickets= {2,3,2};
+    int k = 2;
+    cout << S.timeRequiredToBuy(tickets, k) << endl;
+    return 0;
+}
