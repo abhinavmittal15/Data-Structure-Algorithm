7d79cb2db699e4f95d5bc2e469cc1243511e97ed
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-06-02 00:28:15 +0530

    1

---
+++ LeetCode/1_TwoSum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (map.count(complement)) {
+                return {map[complement], i};
+            }
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> ans = s.twoSum(nums, target);
+    for(int i : ans){
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
