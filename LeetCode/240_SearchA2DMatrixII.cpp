7e8926e7e2769993da02b57efd950268d0164b62
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-06-02 20:15:33 +0530

    240

--- LeetCode/240_SearchA2DMatrixII.cpp
+++ LeetCode/240_SearchA2DMatrixII.cpp
@@ -35,8 +35,8 @@ public:
 int main(){
     Solution s;
     
-    vector<vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
-    int target = 5;
+    vector<vector<int>> matrix = {{1,4},{2,5}};
+    int target = 2;
 
     cout << s.searchMatrix(matrix, target) << endl;
     return 0;
