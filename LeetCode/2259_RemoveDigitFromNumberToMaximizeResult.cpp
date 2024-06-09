2b3d1352c6152b461dede67701220f42547701bc
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-06-09 14:59:38 +0530

    new

--- LeetCode/2259_RemoveDigitFromNumberToMaximizeResult.cpp
+++ LeetCode/2259_RemoveDigitFromNumberToMaximizeResult.cpp
@@ -3,16 +3,15 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    string removeDigit(string number, char digit) {
-        for(int i=0 ; i<number.length(); i++){
-            if(number[i] == digit){
-                number.erase(i, 1);
-                break;
-            }
-        }
-        return number;
+    string removeDigit(string number, char digit)
+    {
+        for (int i = 0; i + 1 < number.length(); ++i)
+            if (number[i] == digit && digit < number[i + 1])
+                return number.erase(i, 1);
+        return number.erase(number.rfind(digit), 1);
     }
 };
 
