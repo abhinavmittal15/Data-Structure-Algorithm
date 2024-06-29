14abc5ccf7b8ff4be4ece0137c7757ad55ca2f64
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-06-30 02:04:54 +0530

    striver linear search

---
+++ Geeks for Geeks/SortedArraySearch.cpp
@@ -0,0 +1,27 @@
+// Link : https://www.geeksforgeeks.org/problems/who-will-win-1587115621/1
+
+#include<iostream>
+using namespace std;
+
+class Solution{
+public:
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+    
+        for(int i=0; i<N; i++){
+            if(arr[i] == K){
+                return 1;
+            }
+        }
+        return -1;
+    }
+};
+
+int main(){
+    Solution s;
+    int N = 5;
+    int K = 6;
+    int arr[N] = {1, 2, 3, 4, 6};
+    cout << s.searchInSorted(arr, N, K) << endl;
+    return 0;
+}
