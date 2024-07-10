ca2af14c4201aa2fe714e806aeef7d6d2b4a4bb0
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-07-10 20:21:36 +0530

    refactor: Remove unnecessary code in WritingSpeed.cpp

---
+++ CodeChef/WritingSpeed.cpp
@@ -0,0 +1,16 @@
+// Link : https://www.codechef.com/START142D/problems/WRITINGSPEED
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if(n*5 <= 60){
+	    cout << "YES";
+	}
+	else{
+	    cout << "NO";
+	}
+    return 0;
+}
