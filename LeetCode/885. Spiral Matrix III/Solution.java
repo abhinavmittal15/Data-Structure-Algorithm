f6943f9e16c4176d917d50423177feb29bd22cbd
Abhinav Mittal <abhinavmittal2003@gmail.com>
2024-08-09 02:31:16 +0530

    refactor: Update LeetCode solution for Spiral Matrix III

---
+++ LeetCode/885. Spiral Matrix III/Solution.java
@@ -0,0 +1,42 @@
+class Solution{
+    public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart){
+        int directions[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+        int n = rows*cols;
+        int res[][] = new int[n][2];
+        res[0][0] = rStart;
+        res[0][1] = cStart;
+        int count = 1;
+        int steps = 1;
+        int index = 0;
+        while(count < n){
+            for(int times = 0; times < 2; times++){
+                int dr = directions[index%4][0];
+                int dc = directions[index%4][1];
+                for(int i=0; i<steps; i++){
+                    rStart += dr;
+                    cStart += dc;
+                    if(rStart >= 0 && rStart < rows && cStart >= 0 && cStart < cols){
+                        res[count][0] = rStart;
+                        res[count][1] = cStart;
+                        count++;
+                    }
+                }
+                index++;
+            }
+            steps++;
+        }
+        return res;
+    }
+    public static void main(String[] args){
+        Solution sol = new Solution();
+        int rows = 1;
+        int cols = 4;
+        int rStart = 0;
+        int cStart = 0;
+        int res[][] = sol.spiralMatrixIII(rows, cols, rStart, cStart);
+        for(int i=0; i<res.length; i++){
+            System.out.println(res[i][0] + " " + res[i][1]);
+        }
+    }
+
+}
