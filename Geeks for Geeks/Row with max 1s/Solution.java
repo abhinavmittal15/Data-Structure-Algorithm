79a19a77f84c9446535c9eeae35f4aa5cd608456
Abhinav Mittal <120883422+abhinavmittal15@users.noreply.github.com>
2024-08-18 15:43:58 +0530

    refactor: Optimize code for finding row with maximum number of 1s

--- Geeks for Geeks/Row with max 1s/Solution.java
+++ Geeks for Geeks/Row with max 1s/Solution.java
@@ -2,27 +2,23 @@ class Solution{
     public int rowWithMax1s(int arr[][]){
         int n = arr.length;
         int m = arr[0].length;
-        int maxCount = 0;
+        int r = 0;
+        int c = m -1;
         int rowIndex = -1;
-        for(int i=0; i<n; i++){
-            int count = 0;
-            for(int j=0; j<m; j++){
-                if(arr[i][j] == 1){
-                    count++;
-                }
+        
+        while(r < n && c >= 0){
+            if(arr[r][c] == 1){
+                rowIndex = r;
+                c--;
             }
-            if(count > maxCount){
-                maxCount = count;
-                rowIndex = i;
+            else{
+                r++;
             }
         }
         return rowIndex;
     }
     public static void main(String[] args){
-        int arr[][] = {{0, 1, 1, 1},
-                       {0, 0, 1, 1},
-                       {1, 1, 1, 1},
-                       {0, 0, 0, 0}};
+        int arr[][] = {{0, 1, 1, 1},{0, 0, 1, 1},{1, 1, 1, 1},{0, 0, 0, 0}};
         Solution obj = new Solution();
         System.out.println(obj.rowWithMax1s(arr));
     }
